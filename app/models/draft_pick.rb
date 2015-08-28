558eea53ca16b1f622d604943ae8c29973ffc4f3
Tim Padjen <tpadjen@gmail.com>
2015-08-27 23:00:26 -0600

    Display draft picks in sidebar

--- app/models/draft_pick.rb
+++ app/models/draft_pick.rb
@@ -1,4 +1,21 @@
 class DraftPick < ActiveRecord::Base
   belongs_to :nfl_player
   belongs_to :fantasy_team
+
+  def selected?
+  	nfl_player != nil
+  end
+
+  def round
+  	r = (number / 10) + 1
+  	number % 10 == 0 ? r - 1 : r
+  end
+
+  def round_pick
+  	number % 10 == 0 ? 10 : number % 10
+  end
+
+  def to_d
+  	"#{round}.#{round_pick}"
+  end
 end
