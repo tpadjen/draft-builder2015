6816d8f54d0fe6547c167d2bce147e32ce0a521a
Tim Padjen <tpadjen@gmail.com>
2015-08-30 15:40:09 -0600

    Refactor

--- app/controllers/fantasy_teams_controller.rb
+++ app/controllers/fantasy_teams_controller.rb
@@ -2,17 +2,10 @@ class FantasyTeamsController < LeaguesViewController
 	around_action :skip_bullet, only: [:show]
 
 	def show
-		puts "In show"
-		p params
-		if !params[:owner]
-			redirect_to(:back)
+		if @fantasy_team = FantasyTeam.from_owner(params[:owner] || nil)
+			@picks, @starters, @bench = @fantasy_team.roster
 		else
-			@fantasy_team = FantasyTeam.from_owner(owner)
-			if @fantasy_team
-				@picks, @starters, @bench = @fantasy_team.roster
-			else
-				redirect_to(:back) 
-			end
+			redirect_to(:back) 
 		end
 	rescue ActionController::RedirectBackError
   	redirect_to root_path
